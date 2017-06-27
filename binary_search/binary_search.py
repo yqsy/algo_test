8691d3685a55ff6a2d5dc2fcc60eef5dd965bbbb
Guochun.Gong <yqsy021@126.com>
2017-06-28 07:26:58 +0800

    按照wiki写了一个binary search,发现只要思路对,写出来的代码没啥大问题..

--- binary_search/binary_search.py
+++ binary_search/binary_search.py
@@ -1,5 +1,32 @@
+# reference: https://en.wikipedia.org/wiki/Binary_search_algorithm
 
+# Although the idea is simple, implementing binary search correctly requires
+# attention to some subtleties about its exit conditions and midpoint calculation.
+
+
+def binary_search(A, T):
+    L = 0
+    R = len(A) - 1
+
+    while L <= R:
+        m = int((L + R) / 2)
+
+        if A[m] < T:
+            L = m + 1
+
+        if A[m] > T:
+            R = m - 1
+
+        if A[m] == T:
+            return m
+
+    return -1
 
 
 if __name__ == '__main__':
-    pass
+    array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    idx = binary_search(array, 4)
+
+    print(idx)
+
