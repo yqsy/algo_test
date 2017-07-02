55e6fa03b53e2c65feaff984c9a18315ef933fdb
Guochun.Gong <yqsy021@126.com>
2017-07-02 22:21:39 +0800

    add convert_figure_to_cn

---
+++ convert_figure_to_cn/convert_figure_to_cn.py
@@ -0,0 +1,8 @@
+def convert_to_cn(num):
+    chn_num_char = ['零', '一', '二', '三', '四', '五', '六', '七', '八', '九']
+    chn_unit_section = ['', '万', '亿', '万亿']
+    chn_unit_char = ['', '十', '百', '千']
+
+
+if __name__ == '__main__':
+    pass
