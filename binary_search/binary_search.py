f8e8245c48a98d31b5ad90e221dc31ba168002e0
Guochun.Gong <yqsy021@126.com>
2017-07-08 20:45:30 +0800

    # test2 和 test4 说明了    while L <= R:中的=号的必要性

--- binary_search/binary_search.py
+++ binary_search/binary_search.py
@@ -6,18 +6,26 @@
 import unittest
 
 
+
 class TestBinarySearch(unittest.TestCase):
     def test_1(self):
         array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-        self.assertEqual(binary_search(array,4), 4)
+        self.assertEqual(binary_search(array, 4), 4)
 
     def test_2(self):
         array = [0]
-        self.assertEqual(binary_search(array,0), 0)
+        self.assertEqual(binary_search(array, 0), 0)
 
     def test_3(self):
         array = []
         self.assertEqual(binary_search(array, 0), None)
+
+    def test_4(self):
+        array = [0, 1, 2]
+        self.assertEqual(binary_search(array, 0), 0)
+
+
+# test2 和 test4 说明了    while L <= R:中的=号的必要性
 
 def binary_search(A, T):
     L = 0
